38cb976f5733a61ce9a62bc5508a73f9b62241ad
lhas <luizhrqas@gmail.com>
2016-09-14 18:39:44 -0300

    dados sendo compartilhados através de um service! :+1:

--- app.js
+++ app.js
@@ -10,19 +10,24 @@
   .module('app')
   .controller('ContainerCtrl', ContainerCtrl);
 
-  ContainerCtrl.$inject = ['$modal', '$scope'];
-  function ContainerCtrl($modal, $scope) {
+  ContainerCtrl.$inject = ['$modal', '$scope', 'User'];
+  function ContainerCtrl($modal, $scope, User) {
     const vm = this;
 
     vm.openModal = openModal;
+    vm.user = User;
 
     // function called when submit is called
     function openModal() {
 
+      // call the service to update the name attribute
+      User.setName(vm.user.name);
+
       // creating the modal instance!
       var modalInstance = $modal.open({
         templateUrl: 'views/modals/add_story.html',
-        controller: 'ModalInstanceCtrl'
+        controller: 'ModalInstanceCtrl',
+        controllerAs: '$ctrl'
       });
 
       // fallbacks from modal here
@@ -43,16 +48,37 @@
   .module('app')
   .controller('ModalInstanceCtrl', ModalInstanceCtrl);
 
-  ModalInstanceCtrl.$inject = ['$scope', '$modalInstance'];
-  function ModalInstanceCtrl($scope, $modalInstance) {
-   
-    $scope.select = function (item) {
-      $modalInstance.close(item);
+  ModalInstanceCtrl.$inject = ['$modalInstance', 'User'];
+  function ModalInstanceCtrl($modalInstance, User) {
+    const vm = this;
+    const user = {
+      name: User.name
     };
 
-    $scope.cancel = function () {
+    vm.user = user;
+    vm.cancel = cancel;
+
+     function cancel() {
       $modalInstance.dismiss('cancel');
     };
+  }
+
+  // user.service.js
+  angular
+  .module('app')
+  .service('User', User);
+
+  function User() {
+    const user = {};
+
+    user.name = 'Luiz';
+
+    // helper used to update the name attribute
+    user.setName = function(newName) {
+      user.name = newName;
+    }
+
+    return user;
   }
 
 
